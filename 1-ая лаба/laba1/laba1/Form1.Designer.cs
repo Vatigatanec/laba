88614c2e6cd9f647612d25e0ed77116fc2d1d5ea
Vatigatanec <sis373737@tut.by>
2017-10-11 22:48:31 +0400

    Get API web

---
+++ 1-ая лаба/laba1/laba1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace laba1
+{
+    partial class Data
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.URL = new System.Windows.Forms.TextBox();
+            this.Get_Data = new System.Windows.Forms.Button();
+            this.Show_Data = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // URL
+            // 
+            this.URL.Location = new System.Drawing.Point(138, 84);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(143, 20);
+            this.URL.TabIndex = 0;
+            this.URL.Text = "http://www.nbrb.by/API/ExRates/Currencies";
+            // 
+            // Get_Data
+            // 
+            this.Get_Data.Location = new System.Drawing.Point(287, 84);
+            this.Get_Data.Name = "Get_Data";
+            this.Get_Data.Size = new System.Drawing.Size(102, 39);
+            this.Get_Data.TabIndex = 1;
+            this.Get_Data.Text = "Получить данные";
+            this.Get_Data.UseVisualStyleBackColor = true;
+            this.Get_Data.Click += new System.EventHandler(this.Get_Data_Click);
+            // 
+            // Show_Data
+            // 
+            this.Show_Data.Location = new System.Drawing.Point(395, 50);
+            this.Show_Data.Multiline = true;
+            this.Show_Data.Name = "Show_Data";
+            this.Show_Data.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Show_Data.Size = new System.Drawing.Size(392, 423);
+            this.Show_Data.TabIndex = 2;
+            // 
+            // Data
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 533);
+            this.Controls.Add(this.Show_Data);
+            this.Controls.Add(this.Get_Data);
+            this.Controls.Add(this.URL);
+            this.Name = "Data";
+            this.Text = "Laba1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox URL;
+        private System.Windows.Forms.Button Get_Data;
+        private System.Windows.Forms.TextBox Show_Data;
+    }
+}
+
---
+++ 1-ая лаба/laba1/laba1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Runtime.Serialization.Json;
+
+namespace laba1
+{
+    public partial class Data : Form
+    {
+        public Data()
+        {
+            InitializeComponent();
+        }
+
+        private void Get_Data_Click(object sender, EventArgs e)
+        {
+            string HtmlText = "";
+            HttpWebRequest myHttwebrequest = (HttpWebRequest)HttpWebRequest.Create(URL.Text);
+            HttpWebResponse myHttpWebresponse = (HttpWebResponse)myHttwebrequest.GetResponse();
+            StreamReader strm = new StreamReader(myHttpWebresponse.GetResponseStream());
+            HtmlText = strm.ReadToEnd();
+            strm.Close();
+            int len = HtmlText.Length;
+            byte[] b = new byte[len];
+            b = Encoding.UTF8.GetBytes(HtmlText);
+            HtmlText = "";
+            HtmlText = Encoding.UTF8.GetString(b);
+            Show_Data.Text = HtmlText;    
+           // DataContractJsonSerializer Interrp = new DataContractJsonSerializer(typeof(List<Сurrency>));
+           // List<Сurrency> cur = (List<Сurrency>)Interrp.ReadObject(HtmlText); 
+           
+        }
+    }
+
+    public class Сurrency
+    {
+        public int Cur_ID { get; set; }
+        public int Cur_ParentID { get; set; }
+        public string Cur_Code { get; set; }
+        public string Cur_Abbreviation { get; set; }
+        public string Cur_Name { get; set; }
+        public string Cur_Name_Bel { get; set; }
+        public string Cur_Name_Eng { get; set; }
+        public string Cur_QuotName { get; set; }
+        public string Cur_QuotName_Bel { get; set; }
+        public string Cur_QuotName_Eng { get; set; }
+        public string Cur_NameMulti { get; set; }
+        public string Cur_Name_BelMulti { get; set; }
+        public string Cur_Name_EngMulti { get; set; }
+        public int Cur_Scale { get; set; }
+        public int Cur_Periodicity { get; set; }
+        public DateTime Cur_DateStart { get; set; }
+        public DateTime Cur_DateEnd { get; set; }
+    }
+
+}
